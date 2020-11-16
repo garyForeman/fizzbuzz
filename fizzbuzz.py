90ffaa1774c499e9b383cc947736133f5935f881
Gary Foreman <gary.foreman@nortonlifelock.com>
2020-11-16 10:24:38 -0700

    Implement divisible by 15 logic.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,8 @@
 class FizzBuzz:
     def parse(self, i):
-        if i % 5 == 0:
+        if i % 15 == 0:
+            return 'fizzbuzz'
+        elif i % 5 == 0:
             return 'buzz'
         elif i % 3 == 0:
             return 'fizz'
